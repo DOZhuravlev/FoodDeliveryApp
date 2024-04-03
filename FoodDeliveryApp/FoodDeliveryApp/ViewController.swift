7e357afe8cd7541d537ddaf2f35fffbbcc456007
Zhuravlev Dmitry <qwerty-91@list.ru>
2024-04-03 15:12:51 +0500

    Created base project, removed storeboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Zhuravlev Dmitry on 03.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
