1666c8c7d819e971862a5a63e29eb9225fd5ac47
Zhuravlev Dmitry <qwerty-91@list.ru>
2024-04-22 14:25:11 +0500

    Added new button to onboarding

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -19,16 +19,36 @@ class OnboardingCoordinator: Coordinator {
 
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [UIViewController]()
-        let firstVC = UIViewController()
-        firstVC.view.backgroundColor = .purple
-        let secondVC = UIViewController()
-        secondVC.view.backgroundColor = .yellow
-        let thirdVC = UIViewController()
-        thirdVC.view.backgroundColor = .red
+        var pages = [OnboardingPartViewController]()
+
+        let firstVC = OnboardingPartViewController()
+        firstVC.imageToShow = UIImage(resource: .chickenLeg1)
+        firstVC.titleText = "Delicious Food"
+        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        firstVC.buttonText = "Next"
+
+        let secondVC = OnboardingPartViewController()
+        secondVC.imageToShow = UIImage(resource: .shipped1)
+        secondVC.titleText = "Fast Shipping"
+        secondVC.descriptionText = "orem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
+        secondVC.buttonText = "Next"
+
+        let thirdVC = OnboardingPartViewController()
+        thirdVC.imageToShow = UIImage(resource: .medal1)
+        thirdVC.titleText = "Certificate Food"
+        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
+        thirdVC.buttonText = "Next"
+
+        let fourthVC = OnboardingPartViewController()
+        fourthVC.imageToShow = UIImage(resource: .creditCard1)
+        fourthVC.titleText = "Payment Online"
+        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
+        fourthVC.buttonText = "Finish"
+
         pages.append(firstVC)
         pages.append(secondVC)
         pages.append(thirdVC)
+        pages.append(fourthVC)
         let presenter = OnboardingViewPresenter(coordinator: self)
         let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
         navigationController?.pushViewController(viewController, animated: true)
--- FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Onboarding/OnboardingViewController.swift
@@ -12,7 +12,8 @@ class OnboardingViewController: UIViewController {
 
     // MARK: - Properties
 
-    private var pages = [UIViewController]()
+    private var pages = [OnboardingPartViewController]()
+    private let bottomButton = UIButton()
 
     // MARK: - Views
 
@@ -20,7 +21,7 @@ class OnboardingViewController: UIViewController {
     private let pageControl = UIPageControl()
     weak var viewOutput: OnboardingViewOutput!
 
-    init(pages: [UIViewController] = [UIViewController](), viewOutput: OnboardingViewOutput!) {
+    init(pages: [OnboardingPartViewController] = [OnboardingPartViewController](), viewOutput: OnboardingViewOutput!) {
         self.pages = pages
         self.viewOutput = viewOutput
         super.init(nibName: nil, bundle: nil)
@@ -34,7 +35,31 @@ class OnboardingViewController: UIViewController {
         super.viewDidLoad()
         setupPageViewController()
         setupPageControl()
+        setupButton()
+    }
+}
 
+// MARK: - Actions
+private extension OnboardingViewController {
+    @objc func buttonPressed() {
+        switch pageControl.currentPage {
+        case 0:
+            pageControl.currentPage = 1
+            pageViewController.setViewControllers([pages[1]], direction: .forward, animated: true)
+            bottomButton.setTitle(pages[1].buttonText, for: .normal)
+        case 1:
+            pageControl.currentPage = 2
+            pageViewController.setViewControllers([pages[2]], direction: .forward, animated: true)
+            bottomButton.setTitle(pages[2].buttonText, for: .normal)
+        case 2:
+            pageControl.currentPage = 3
+            pageViewController.setViewControllers([pages[3]], direction: .forward, animated: true)
+            bottomButton.setTitle(pages[3].buttonText, for: .normal)
+        case 3:
+           print("Exit")
+        default:
+            break
+        }
     }
 }
 
@@ -44,6 +69,7 @@ private extension OnboardingViewController {
     func setupPageViewController() {
         pageViewController.delegate = self
         pageViewController.dataSource = self
+        pageViewController.view.backgroundColor = AppColors.accentOrange
 
         pageViewController.setViewControllers([pages.first!], direction: .forward, animated: true)
 
@@ -54,6 +80,10 @@ private extension OnboardingViewController {
     func setupPageControl() {
         pageControl.numberOfPages = pages.count
         pageControl.currentPage = 0
+        let page = pages[0]
+        let title = page.buttonText
+        bottomButton.setTitle(title, for: .normal)
+        pageControl.isUserInteractionEnabled = false
 
         pageControl.translatesAutoresizingMaskIntoConstraints = false
 
@@ -61,9 +91,24 @@ private extension OnboardingViewController {
 
         NSLayoutConstraint.activate([
             pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30)
+            pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -45)
 
         ])
+    }
+    func setupButton() {
+        view.addSubview(bottomButton)
+        bottomButton.translatesAutoresizingMaskIntoConstraints = false
+        bottomButton.backgroundColor = AppColors.grey
+        bottomButton.titleLabel?.font = .Roboto.bold.size(of: 18)
+        bottomButton.setTitleColor(AppColors.black, for: .normal)
+        bottomButton.layer.cornerRadius = 16
+
+        NSLayoutConstraint.activate([
+            bottomButton.bottomAnchor.constraint(equalTo: pageControl.bottomAnchor, constant: -44),
+            bottomButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            bottomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            bottomButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
 }
 
@@ -72,13 +117,13 @@ private extension OnboardingViewController {
 // MARK: - UIPageViewControllerDataSource
 extension OnboardingViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let currentIndex = pages.firstIndex(of: viewController), currentIndex > 0 else { return UIViewController() }
+        guard let currentIndex = pages.firstIndex(of: viewController as! OnboardingPartViewController), currentIndex > 0 else { return nil }
         return pages[currentIndex - 1]
 
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let currentIndex = pages.firstIndex(of: viewController), currentIndex < pages.count - 1 else { return UIViewController() }
+        guard let currentIndex = pages.firstIndex(of: viewController as! OnboardingPartViewController), currentIndex < pages.count - 1 else { return nil }
         return pages[currentIndex + 1]
     }
 
@@ -87,8 +132,12 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
 // MARK: - UIPageViewControllerDelegate
 extension OnboardingViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-        if let index = pages.firstIndex(of: pendingViewControllers.first!) {
+        if let index = pages.firstIndex(of: pendingViewControllers.first! as! OnboardingPartViewController) {
             pageControl.currentPage = index
+            let page = pages[index]
+            let title = page.buttonText
+            bottomButton.setTitle(title, for: .normal)
+
         }
     }
 
